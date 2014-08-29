1eff36cbb1715fe496e7cdb3f2b18b3f3b09b9aa
Doug Walter <doug@kosmojo.com>
2014-08-29 16:50:28 -0700

    working scroll

---
+++ anim.js
@@ -0,0 +1,24 @@
+<h1 id="ch">Coding House</h1>
+
+
+<style>
+
+
+</style>
+
+    <script>
+    var e = document.getElementsById("ch")
+
+    e.style.color = "green";
+    e.style.position = "fixed";
+    e.style.top = "50%";
+
+    e.text(0);
+
+
+    </script>
+
+
+
+
+
